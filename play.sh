c4d9386a1f84e925b2c448e44c70e23d51231ad0
Anthony Poinsot <anypot@posteo.net>
2020-12-04 17:38:05 +0100

    Add synology nginx tasks

--- play.sh
+++ play.sh
@@ -3,7 +3,9 @@
 export ANSIBLE_USER=
 export ANSIBLE_SSH_PASS=
 export ANSIBLE_BECOME_PASS=
-export NAS_PORT=
+export NAS_SSH_PORT=
+export NAS_NEW_80_PORT=
+export NAS_NEW_443_PORT=
 export PUBKEY_FILE=
 
 ansible-playbook site.yml
