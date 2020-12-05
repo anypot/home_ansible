5984bf946e8c4e7cdf85870cd859916b21890ef4
Anthony 'anypot' Poinsot <anypot@posteo.net>
2020-12-05 01:50:32 +0100

    Shell optimization

--- play.sh
+++ play.sh
@@ -1,11 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-export ANSIBLE_USER=
-export ANSIBLE_SSH_PASS=
-export ANSIBLE_BECOME_PASS=
-export NAS_SSH_PORT=
-export NAS_NEW_80_PORT=
-export NAS_NEW_443_PORT=
-export PUBKEY_FILE=
+CHECK=0
 
-ansible-playbook site.yml
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	-c|--check) CHECK=1 ;;
+	*) echo "Usage: play.sh -c|--check"; exit 1 ;;
+	esac
+	shift
+done
+
+source ./env.sh
+
+if [[ ${CHECK} == 1 ]]; then
+	ansible-playbook site.yml --check
+else
+	ansible-playbook site.yml
+fi
