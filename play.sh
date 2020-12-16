71b1237257cfae7b2c5af8008add25db07a47c25
Anthony 'anypot' Poinsot <anypot@posteo.net>
2020-12-16 01:08:43 +0100

    Shell improvement

--- play.sh
+++ play.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 CHECK=0
+ASK_PASSWORD=0
+CMD_ARGS=""
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 	-c|--check) CHECK=1 ;;
-	*) echo "Usage: play.sh -c|--check"; exit 1 ;;
+	-p|--password) ASK_PASSWORD=1 ;;
+	*) echo "Usage: play.sh -c|--check -p|--password"; exit 1 ;;
 	esac
 	shift
 done
 
 source ./env.sh
 
-if [[ ${CHECK} == 1 ]]; then
-	ansible-playbook site.yml --check
-else
-	ansible-playbook site.yml
-fi
+[[ ${ASK_PASSWORD} == 1 ]] && CMD_ARGS="${CMD_ARGS} --ask-pass"
+[[ -z ${ANSIBLE_BECOME_PASS+x} ]] && CMD_ARGS="${CMD_ARGS} --ask-become-pass"
+[[ ${CHECK} == 1 ]] && CMD_ARGS="${CMD_ARGS} --check"
+
+ansible-playbook site.yml ${CMD_ARGS}
